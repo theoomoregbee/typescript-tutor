fbbf0b22e207ed4c02e21e14b0d35b06b1041242
Theophilus Omoregbee <theo4u@ymail.com>
2016-08-01 16:19:50 +0100

    with error buh still generates the javascript despite the error from the compilation just to let the user know that the program might misbehave

--- greeter.js
+++ greeter.js
@@ -4,6 +4,6 @@
 function greeter(person) {
     return "Hello, " + person;
 }
-var user = "Jane User";
+var user = [0, 1, 2]; //"Jane User";
 document.body.innerHTML = greeter(user);
 //# sourceMappingURL=greeter.js.map
--- greeter.ts
+++ greeter.ts
@@ -6,6 +6,6 @@ function greeter(person: string){
     return "Hello, "+ person;
 }
 
-var user = "Jane User";
+var user = [0, 1, 2];//"Jane User";
 
 document.body.innerHTML = greeter(user);

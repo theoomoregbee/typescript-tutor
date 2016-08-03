c3d3abd5d201069c79c8a7f2d6340e237bb67e46
Theophilus Omoregbee <theo4u@ymail.com>
2016-08-03 15:22:58 +0100

    working with classes

--- greeter.js
+++ greeter.js
@@ -1,9 +1,18 @@
 /**
  * Created by SQ04 on 8/1/2016.
  */
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
 //# sourceMappingURL=greeter.js.map
--- greeter.ts
+++ greeter.ts
@@ -2,6 +2,14 @@
  * Created by SQ04 on 8/1/2016.
  */
 
+class Student{
+    fullName: string;
+
+    constructor(public firstName, public middleInitial, public lastName){
+        this.fullName = firstName + " "+middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -11,6 +19,5 @@ function greeter(person: Person){
     return "Hello, "+ person.firstName + " "+person.lastName;
 }
 
-var user = { firstName:"Jane", lastName: "User" };
-
+var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
